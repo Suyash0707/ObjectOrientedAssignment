05f162ca1f5408a231292b36cc007bdb6cfbebe7
Suyash0707 <suyashb175@gmail.com>
2023-03-04 17:23:12 +0530

    Add project files.

---
+++ ObjectOrientedAssignment/Program.cs
@@ -0,0 +1,92 @@
+﻿
+using ObjectOrientedAssignment;
+    
+    public class program
+{
+    public static void Main()
+    {
+        Student student1 = new Student();
+
+        Console.WriteLine("Enter Student1 Roll No:");
+        student1.roll = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter Student1 Name No:");
+        student1.name = Convert.ToString(Console.ReadLine());
+
+        Console.WriteLine("Enter Student1 Age:");
+        student1.age = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter Student1 Address:");
+        student1.address = Convert.ToString(Console.ReadLine());
+
+        Console.WriteLine("Student1 Roll.no:" + student1.roll);
+        Console.WriteLine("Student1 Name:" + student1.name);
+        Console.WriteLine("Student1 Age:" + student1.age);
+        Console.WriteLine("Student1 Address:" + student1.address);
+        Console.WriteLine(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+
+        Student student2 = new Student();
+
+        Console.WriteLine("Enter Student2 Roll No:");
+        student2.roll = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter Student2 Name No:");
+        student2.name = Convert.ToString(Console.ReadLine());
+
+        Console.WriteLine("Enter Student2 Age:");
+        student2.age = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter Student2 Address:");
+        student2.address = Convert.ToString(Console.ReadLine());
+
+        Console.WriteLine("Student2 Roll.no:" + student2.roll);
+        Console.WriteLine("Student2 Name:" + student2.name);
+        Console.WriteLine("Student2 Age:" + student2.age);
+        Console.WriteLine("Student2 Address:" + student2.address);
+        Console.WriteLine(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+
+
+        Student student3 = new Student();
+
+        Console.WriteLine("Enter Student3 Roll No:");
+        student3.roll = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter Student3 Name No:");
+        student3.name = Convert.ToString(Console.ReadLine());
+
+        Console.WriteLine("Enter Student3 Age:");
+        student3.age = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter Student3 Address:");
+        student3.address = Convert.ToString(Console.ReadLine());
+
+        Console.WriteLine("Student3 Roll.no:" + student3.roll);
+        Console.WriteLine("Student3 Name:" + student3.name);
+        Console.WriteLine("Student3 Age:" + student3.age);
+        Console.WriteLine("Student3 Address:" + student3.address);
+        Console.WriteLine(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+
+
+        Student student4 = new Student();
+
+        Console.WriteLine("Enter Student4 Roll No:");
+        student4.roll = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter Student4 Name No:");
+        student4.name = Convert.ToString(Console.ReadLine());
+
+        Console.WriteLine("Enter Student4 Age:");
+        student4.age = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter Student4 Address:");
+        student4.address = Convert.ToString(Console.ReadLine());
+
+        Console.WriteLine("Student4 Roll.no:" + student4.roll);
+        Console.WriteLine("Student4 Name:" + student4.name);
+        Console.WriteLine("Student4 Age:" + student4.age);
+        Console.WriteLine("Student4 Address:" + student4.address);
+        Console.WriteLine(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+
+
+    }
+}
---
+++ ObjectOrientedAssignment/Student.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedAssignment
+{
+    internal class Student
+    {
+        public int roll  { get; set; } 
+        public string name { get; set; }        
+
+        public int age { get; set; }
+
+        public string address { get; set; }
+
+        public void GetData()
+        {
+            Console.WriteLine("Please enter the value for Student Roll No:");
+            this.roll = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the value for Student Name:");
+            this.name = Console.ReadLine();
+            Console.WriteLine("Please enter the value for Student Age:");
+            this.age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the value for Student Address:");
+            this.address = Console.ReadLine();
+        }
+
+        public void PutData()
+        {
+            Console.WriteLine("Roll no = " + this.roll);
+            Console.WriteLine("Name = " + this.name);
+            Console.WriteLine("Age = " + this.age);
+            Console.WriteLine("Address=" + this.address);
+        
+        }
+
+
+    }
+}
